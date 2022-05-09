3551e0bd3a0cbc36651f13748e6ffb6ac39ab53e
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-05-09 19:39:33 +0300

    displaying model names

--- to_doo/models.py
+++ to_doo/models.py
@@ -6,7 +6,12 @@ class Tasks(models.Model):
     created = models.DateField ()
     cat = models.ForeignKey ('Category', on_delete = models.PROTECT)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField (max_length=100, db_index=True)
 
+    def __str__(self):
+        return self.name
