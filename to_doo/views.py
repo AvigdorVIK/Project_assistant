3dfab5268068dae799ba400f712f50e5f74edd22
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-05-11 13:09:19 +0300

    add_task form and html

--- to_doo/views.py
+++ to_doo/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index (request):
     return render(request, 'to_doo/index.html')
+
+def add_task (request):
+    return render (request,  'to_doo/add_task.html')
