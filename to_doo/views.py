babae897da5ad197b216d518003c00cf019a2ff4
AvigdorVIK <kanynnikovvik19@gmail.com>
2022-05-15 17:10:56 +0300

    logek in form

--- to_doo/views.py
+++ to_doo/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from to_doo.forms import *
 
 
 def index (request):
     return render(request, 'to_doo/index.html')
 
 def add_task (request):
-    return render (request,  'to_doo/add_task.html')
+    if request.method == 'POST':
+        form = AddTasksForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('home')
+            except:
+                form.edd_error()
+                return redirect(None, 'not valid')
+
+    else:
+        form = AddTasksForm()
+    return render (request,  'to_doo/add_task.html', {'form': form})
